ecaa2f0fcc4dd0c6c0049ed1d71fdaefd1b9fe59
Nikolas Kougioulis <nkougioulis95@gmail.com>
2023-08-24 12:02:56 +0300

    small fix

--- sieve.py
+++ sieve.py
@@ -1,9 +1,9 @@
 # Sieve of Eratosthenes of Cyrene
 def sieve(n):
-    sieve_list = [True]*n #boolean list of length n to keep track of the sieve
+    sieve_list = [True]*(n+1) #boolean list of length n to keep track of the sieve
     for i in range(2, int(n**0.5 + 1)):
         if sieve_list[i] == True:
-            for j in range(i*i, n, i): #start crossing out from the sieve
+            for j in range(i*i, n+1, i): #start crossing out from the sieve
                 sieve_list[j] = False
    
     primes = []
